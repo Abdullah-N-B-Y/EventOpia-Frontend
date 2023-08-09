c654550774a805fcdeea766b485c849a33319afe
SumWutChill <mramzey@ymail.com>
2023-08-09 15:32:25 +0300

    Implemented register component

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { LoginDTO } from '../shared/DTO/LoginDTO';
 import { endPointURL } from 'src/constants/constants';
 import { Observable } from 'rxjs';
+import { RegisterDTO } from '../shared/DTO/RegisterDTO';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,11 @@ export class AuthService {
   private serviceURL = endPointURL + 'Auth/';
   constructor(private http: HttpClient) {}
 
-  login(loginDTO: LoginDTO): Observable<string> {
+  login(loginDTO: LoginDTO): Observable<any> {
     return this.http.post<string>(this.serviceURL + 'Login', loginDTO);
+  }
+
+  register(registerDTO: RegisterDTO): Observable<any> {
+    return this.http.post<string>(this.serviceURL + 'Register', registerDTO);
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,93 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/Services/auth.service';
+import { LoginDTO } from 'src/app/shared/DTO/LoginDTO';
+import { RegisterDTO } from 'src/app/shared/DTO/RegisterDTO';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  focus: boolean | undefined;
+  focus1: boolean | undefined;
+  focus2: boolean | undefined;
+  showValidations: boolean = false;
+  registerSuccess: boolean = false;
+  usernameAlreadyExists: boolean = false;
+  emailAlreadyExists: boolean = false;
 
+  constructor(private authService: AuthService) {}
+
+  private passwordPattern: RegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/;
+
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.passwordPattern),
+      Validators.minLength(8),
+      Validators.maxLength(50),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(50),
+    ]),
+  });
+
+  register(): void {
+    this.showValidations = true
+    this.usernameAlreadyExists = false;
+    this.emailAlreadyExists = false;
+    if (!this.registerForm.valid) return;
+
+    let registerDTO: RegisterDTO = {
+      Username: this.registerForm.get('username')?.value,
+      Password: this.registerForm.get('password')?.value,
+      Email: this.registerForm.get('email')?.value,
+    };
+
+    this.authService.register(registerDTO).subscribe(
+      (res: any) => {
+        this.registerSuccess = true;
+        console.log(res);
+      },
+      (error: HttpErrorResponse) => {
+        this.registerSuccess = false;
+        if (error.error && error.error.error) {
+          if (error.error.error === 'username already exists') {
+            this.usernameAlreadyExists = true;
+          }
+          if (error.error.error === 'email already exists') {
+            this.emailAlreadyExists = true;
+          }
+        } else {
+          console.log(error);
+        }
+      }
+    );
+  }
+
+  ngOnInit() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('login-page');
+
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
+  }
+  ngOnDestroy() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('login-page');
+
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.remove('navbar-transparent');
+  }
 }
