72a36280315238d4477a60ab63af43c739482aba
Ahmad Hisham <AhmadHTK.96@outlook.com>
2023-08-14 15:16:06 +0300

    statistics and report done

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { SharedModule } from './shared/shared.module';
-
-
+import { StatisticsComponent } from './statistics/statistics.component';
+import { FormsModule } from '@angular/forms';
+import { ReportComponent } from './report/report.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
-  declarations: [
-    ProfileComponent
-  ],
+  declarations: [ProfileComponent, StatisticsComponent, ReportComponent],
   imports: [
     CommonModule,
     DashboardModule,
-    SharedModule
-  ]
+    SharedModule,
+    FormsModule,
+    NgChartsModule,
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}
--- src/app/admin/dashboard/dashboard.module.ts
+++ src/app/admin/dashboard/dashboard.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    HomeDashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    SharedModule
-  ]
+  declarations: [HomeDashboardComponent],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule, FormsModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
