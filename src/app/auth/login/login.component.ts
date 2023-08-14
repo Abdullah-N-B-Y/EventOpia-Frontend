a23859d36ee1ef2fc43a3339e4f941b2e4f250d2
SumWutChill <mramzey@ymail.com>
2023-08-14 21:10:01 +0300

    Added create profile for new user

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
 import { LoginDTO } from 'src/app/shared/DTO/LoginDTO';
+import { Profile } from 'src/app/shared/Data/Profile';
 
 @Component({
     selector: 'app-login',
@@ -19,7 +23,13 @@ export class LoginComponent implements OnInit {
     stored_username: string | null = localStorage.getItem('username_Eventopia');
     stored_password: string | null = localStorage.getItem('password_Eventopia');
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private matDialog: MatDialog,
+        private toastr: ToastrService,
+        private profileService: ProfileService
+    ) {}
 
     private passwordPattern: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/;
 
@@ -50,16 +60,23 @@ export class LoginComponent implements OnInit {
                 this.validCredentials = true;
                 this.loginSuccess = true;
                 let decodedToken: any = jwtDecode(res.content);
+                let roleId: string = decodedToken['RoleId'];
+                let userId: string = decodedToken['UserId'];
                 localStorage.setItem('jwtToken', res.content);
-                localStorage.setItem('UserId', decodedToken['UserId']);
+                localStorage.setItem('UserId', userId);
+                localStorage.setItem('RoleId', roleId);
                 localStorage.setItem('Username', decodedToken['Username']);
-                localStorage.setItem('RoleId', decodedToken['RoleId']);
                 console.log('res is here', res.content);
                 if (this.loginForm.get('rememberMe')?.value === true) this.rememberMeOn();
 
-                let roleId: string = decodedToken['RoleId'];
-                if (roleId === '1') this.router.navigate(['/admin/home']);
-                else if (roleId === '2') this.router.navigate(['/']);
+                let hasProfile: boolean;
+                this.profileService.getProfileByUserId(parseInt(userId, 10)).subscribe((res) => {
+                    if (roleId === '1') this.router.navigate(['/admin/home']);
+                    else if (roleId === '2') this.router.navigate(['/']);
+                }, err => {
+                    if (err.status === 404)
+                        this.openAddProfileDialog();
+                });
             },
             (err) => {
                 this.validCredentials = false;
@@ -81,6 +98,74 @@ export class LoginComponent implements OnInit {
 
     navigateToRegister() {
         this.router.navigate(['/auth/register']);
+    }
+
+    private phoneNumberPattern: RegExp = /^\+?[0-9]{10,13}$/;
+    profileForm: FormGroup = new FormGroup({
+        firstName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+        lastName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+        phoneNumber: new FormControl('', [Validators.required, Validators.pattern(this.phoneNumberPattern)]),
+        gender: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+        dateOfBirth: new FormControl('', [Validators.required]),
+        bio: new FormControl('', [Validators.required, Validators.maxLength(500)]),
+        profileImage: new FormControl(null, [Validators.required]),
+    });
+    newProfileImage: File | undefined;
+    @ViewChild('addProfileDialog') addProfileDialog!: TemplateRef<any>;
+    dialogRef!: any;
+    openAddProfileDialog() {
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.width = '500px'; // Set the desired width here
+        dialogConfig.disableClose = true;
+        this.dialogRef = this.matDialog.open(this.addProfileDialog, dialogConfig);
+        this.dialogRef.afterClosed().subscribe((result: string | undefined) => {
+            if (result != undefined) {
+                if (result == 'save') {
+                    // this.changePassword();
+                }
+            }
+        });
+    }
+
+    onAddProfileSubmit() {
+        if (!this.newProfileImage) {
+            console.log('no image provided'); //add toastr
+            return;
+        }
+
+        const userId: string | null = localStorage.getItem('UserId');
+        if (!userId) {
+            console.log('userId from local storage is null');
+            return;
+        }
+        const newProfile: Profile = {
+            firstName: this.profileForm.get('firstName')?.value,
+            lastName: this.profileForm.get('lastName')?.value,
+            phoneNumber: this.profileForm.get('phoneNumber')?.value,
+            gender: this.profileForm.get('gender')?.value,
+            dateOfBirth: this.profileForm.get('dateOfBirth')?.value,
+            bio: this.profileForm.get('bio')?.value,
+            rate: 0,
+            userId: parseInt(userId, 10),
+            receivedImageFile: this.newProfileImage,
+        };
+        console.log('new profile: ');
+        console.log(newProfile);
+        this.profileService.createProfile(newProfile).subscribe(
+            (res) => {
+                console.log('create profile has succeeded:', res);
+                this.dialogRef.close();
+                this.toastr.success('Successfully created profile');
+            },
+            (err) => {
+                console.log('create profile error:', err);
+                this.toastr.error('create profile has failed');
+            }
+        );
+    }
+
+    onFileChange(event: any): void {
+        this.newProfileImage = event.target.files[0];
     }
 
     ngOnInit() {
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -6,70 +6,87 @@ import { Observable } from 'rxjs/internal/Observable';
 import { Profile } from '../shared/Data/Profile';
 import { catchError, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { createFormDataFromObject } from '../shared/utilities/createFormDataFromObject';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ProfileService {
+    constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  updateProfile (body:any): Observable<boolean> {
-      return this.http.put('https://localhost:7189/api/Profile/UpdateProfile',body).pipe(
-        map((resp: any) => {
-          console.log('Password changed successfully', resp);
-          return true;
-        }),
-        catchError((err) => {
-          console.error('Error changing password', err.messageReceivers);
-          return of(false);
-        })
-      );
-  }
-  changePassword(body: any, id: number): Observable<boolean> {
-    return this.http.put(endPointURL + `User/UpdatePassword/${id}`, body).pipe(
-      map((resp: any) => {
-        console.log('Password changed successfully', resp);
-        return true;
-      }),
-      catchError((err) => {
-        console.error('Error changing password', err.messageReceivers);
-        return of(false);
-      })
-    );
-  }
-
-  getUserById(id: number): Observable<User> {
-    return this.http.get<User>(endPointURL + `User/GetUserById/${id}`);
-  }
-  
-  getProfileByUserId(id: number): Observable<Profile> {
-    return new Observable<Profile>(observer => {
-      this.http.get<Profile>(endPointURL + `Profile/GetProfileByUserId/${id}`)
-        .subscribe(
-          (response: Profile) => {
-            console.log(response);
-            observer.next(response);
-            observer.complete();
-          },
-          (error: any) => {
-            console.error('An error occurred:', error);
-            observer.error(error);
-          }
+    updateProfile(body: any): Observable<boolean> {
+        return this.http.put(endPointURL + 'Profile/UpdateProfile', body).pipe(
+            map((resp: any) => {
+                console.log('Profile updated successfully', resp);
+                return true;
+            }),
+            catchError((err) => {
+                console.error('Error updating profile', err.messageReceivers);
+                return of(false);
+            })
         );
-    });
-  }
+    }
+  createProfile(profile: Profile): Observable<boolean> {
+    const formData: FormData = createFormDataFromObject(profile);
+        return this.http.post(endPointURL + 'Profile/CreateProfile', formData).pipe(
+            map((resp: any) => {
+                console.log('Profile created successfully', resp);
+                return true;
+            }),
+            catchError((err) => {
+                console.error('Error creating profile', err.messageReceivers);
+                return of(false);
+            })
+        );
+    }
 
-  updateProfileImage(userId:number, image: any):void{
-    this.http.post(`https://localhost:7189/api/Profile/UploadImage/${userId}`,image).toPromise().then(
-      res => {
-        console.log(res); 
-      },err=>{
-        console.log(err)
-      }
-    )
-  }
-  getProfileImage(id: number): Observable<File> {
-    return this.http.get<File>(endPointURL + `Profile/GetProfileImage/${id}`);
-  }
+    changePassword(body: any, id: number): Observable<boolean> {
+        return this.http.put(endPointURL + `User/UpdatePassword/${id}`, body).pipe(
+            map((resp: any) => {
+                console.log('Password changed successfully', resp);
+                return true;
+            }),
+            catchError((err) => {
+                console.error('Error changing password', err.messageReceivers);
+                return of(false);
+            })
+        );
+    }
+
+    getUserById(id: number): Observable<User> {
+        return this.http.get<User>(endPointURL + `User/GetUserById/${id}`);
+    }
+
+    getProfileByUserId(id: number): Observable<Profile> {
+        return new Observable<Profile>((observer) => {
+            this.http.get<Profile>(endPointURL + `Profile/GetProfileByUserId/${id}`).subscribe(
+                (response: Profile) => {
+                    console.log(response);
+                    observer.next(response);
+                    observer.complete();
+                },
+                (error: any) => {
+                    console.error('An error occurred:', error);
+                    observer.error(error);
+                }
+            );
+        });
+    }
+
+    updateProfileImage(userId: number, image: any): void {
+        this.http
+            .post(endPointURL + `Profile/UploadImage/${userId}`, image)
+            .toPromise()
+            .then(
+                (res) => {
+                    console.log(res);
+                },
+                (err) => {
+                    console.log(err);
+                }
+            );
+    }
+    getProfileImage(id: number): Observable<File> {
+        return this.http.get<File>(endPointURL + `Profile/GetProfileImage/${id}`);
+    }
 }
