d51aa087a40b90d187b24884404117b01c992741
Abdullah N Bani Yassin <anby.githup@gmail.com>
2023-08-16 08:31:36 +0300

    make page dynamic

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -5,6 +5,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { SucceededDialogComponent } from '../shared/dynamic-dialoges/succeeded-dialog/succeeded-dialog.component';
 import { FailedDialogComponent } from '../shared/dynamic-dialoges/failed-dialog/failed-dialog.component';
 import { ContactUsEntry } from '../shared/Data/ContactUsEntry';
+import { PageService } from '../services/page.service';
+import { Page } from '../shared/Data/Page';
 
 @Component({
   selector: 'app-contact-us',
@@ -12,8 +14,22 @@ import { ContactUsEntry } from '../shared/Data/ContactUsEntry';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  //This is a third page or contact-us page with id = 3
 
-  constructor(private contactUs: ContactUsService, private dialog:MatDialog){}
+  constructor(private contactPage: PageService,private contactUs: ContactUsService, private dialog:MatDialog){}
+  page: Page = {
+    id: 3
+  };
+  ngOnInit(): void {
+    this.contactPage.getPageById(3).subscribe(
+      (res)=>{
+        this.page = res;
+      },(err)=>{
+        console.log(`err=> ${err.message}`)
+      }
+    );
+  }   
+
   contactUsForm: FormGroup = new FormGroup({
     email : new FormControl('',Validators.required),
     phoneNumber : new FormControl(''),
@@ -40,4 +56,6 @@ export class ContactUsComponent {
     });;
   }
   
+
+
 }
--- src/app/user/user-home/user-home.component.ts
+++ src/app/user/user-home/user-home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageService } from 'src/app/services/page.service';
+import { Page } from 'src/app/shared/Data/Page';
 
 @Component({
   selector: 'app-user-home',
   templateUrl: './user-home.component.html',
   styleUrls: ['./user-home.component.css']
 })
-export class UserHomeComponent {
+export class UserHomeComponent implements OnInit{
+  //This is a first page or home page with id = 1
 
+  constructor(private homePage: PageService){}
+  page: Page = {
+    id: 1
+  };
+  ngOnInit(): void {
+    this.homePage.getPageById(1).subscribe(
+      (res)=>{
+        this.page = res;
+      },(err)=>{
+        console.log(`err=> ${err.message}`)
+      }
+    );
+  }   
 }
